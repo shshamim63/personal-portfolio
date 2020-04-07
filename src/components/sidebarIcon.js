f90922d01e0acc705bd7ad1be884c3ddd66f7275
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-04-07 21:53:58 +0600

    :art: Fixes issues mentioned for opening new tab
    Opens new tab when clicks on the sidebar social icons.

--- src/components/sidebarIcon.js
+++ src/components/sidebarIcon.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const SidebarIcon = () => (
   <div className="icon-bar">
-    <a href="https://twitter.com/Shshamim090" className="twitter">
+    <a href="https://twitter.com/Shshamim090" className="twitter" target="_blank" rel="noopener noreferrer">
       <i class="fa fa-twitter"></i>
     </a> 
-    <a href="mailto:shakhawathossain032@gmail.com" className="gmail">
+    <a href="mailto:shakhawathossain032@gmail.com" className="gmail" target="_blank" rel="noopener noreferrer">
       <i class="fa fa-envelope"></i>
     </a> 
-    <a href="https://www.linkedin.com/in/shakhawathossainshamim/" className="linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="https://github.com/shshamim63" className="github"><i class="fa fa-github"></i></a>
+    <a href="https://www.linkedin.com/in/shakhawathossainshamim/" className="linkedin" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin"></i></a>
+    <a href="https://github.com/shshamim63" className="github" target="_blank" rel="noopener noreferrer"><i class="fa fa-github"></i></a>
   </div>
 );
 export default SidebarIcon;
