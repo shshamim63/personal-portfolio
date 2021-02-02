b737ee269bc0fbd1ebd18d389fb1453b30743774
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2021-02-02 20:58:14 +0600

    Updated the bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,11 +12,15 @@ const AboutMe = () => (
           <div className="section-heading">
             <h2>About <strong>Me</strong></h2>
           </div>
-          <p className="lead">I am currently working remotely as a technical support engineer for Microverse, where I review code and provide technical guidance for web development students in HTML & CSS and Ruby. I love to watch animation movies and coffee drinker.</p>
-          <p className="mb-5  ">I have worked with Ruby on Rails, JavaScript, React and Redux, and have build some awesome projects.</p>
+          <p className="lead">Hi, I'm Shamim, a multi-talented human with over 3+ years of experiences in a wide range of web-development disciplines</p>
+          <p className="lead-bio">I've done remote work for a US-based company, mentored new developers, collaborated with talented people around the world to create digital products for both business and consumer use. I don’t like to define myself by the work I’ve done. I define myself by the work I want to do. Skills can be taught, personality is inherent. I prefer to keep learning, continue challenging myself and do interesting things that matter.
+          I am currently participating in open source projects and am open to new opportunities.
+			    </p>
+          <p className="mb-5">You can read more about my biography, experience, skills, education and much more by downloading the CV.
+			    </p>
           <p>
             <a href="#section-contact" className="btn btn-primary px-4 py-2 btn-sm smoothscroll">Hire Me</a>
-            <a href={require('../document/Resume_Shakawat.pdf')} className="btn btn-secondary px-4 py-2 btn-sm" download>Download CV</a>
+            <a href={require('../document/Hossain_Shakhawat.pdf')} className="btn btn-secondary px-4 py-2 btn-sm" download>Download CV</a>
           </p>
         </div>
       </div>
